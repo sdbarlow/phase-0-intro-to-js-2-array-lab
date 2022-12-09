09f9d881a5be4038bf1657a89f7577eb88c6a8d6
Seth Barlow <sethdbarlow@gmail.com>
2022-12-09 09:57:44 -0700

    commit tree fa5717882975a1463d16c3392a48f6671ecc6f17

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newArr = [...cats, name]
+    return newArr;
+}
+
+function prependCat(name) {
+    const newArr = [name, ...cats];
+    return newArr;
+}
+
+function removeLastCat() {
+    const newArr = cats.slice(0, -1);
+    return newArr;
+}
+
+function removeFirstCat() {
+    const newArr = cats.slice(1, cats.length);
+    return newArr;
+}
+
