c65ec184dc08bfb9052c67957193df6d6b59b1cd
Seth Barlow <sethdbarlow@gmail.com>
2023-01-31 10:37:21 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(id){
+    cats.push(id);
+ return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    const newArr = [...cats, name]
+    return newArr;
+}
+
+function prependCat(name) {
+    const newArr = [name, ...cats];
+    return newArr;
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0,2);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
